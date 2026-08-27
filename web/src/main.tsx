89ca0b7e6fa29d21c7a664d417c06249ca860150
ribo-project <ribo@job2u.com.tw>
2026-08-27 10:54:45 +0800

    feat: mount Linear integration UI

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { LinearIntegrationExtension } from "./components/LinearIntegrationExtension";
+import {
+  resolveTaskboardLanguage,
+  TaskboardLanguageProvider,
+} from "./i18n";
 import { initializeTaskboardStorage } from "./storage";
 import "./styles.css";
+import "./linearIntegration.css";
 
 async function main() {
   await initializeTaskboardStorage();
+  const query = new URL(document.baseURI).searchParams;
+  const language = resolveTaskboardLanguage(query.get("lang") ?? navigator.language);
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
+      <TaskboardLanguageProvider language={language}>
+        <LinearIntegrationExtension />
+      </TaskboardLanguageProvider>
     </StrictMode>,
   );
 }
